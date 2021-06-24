58db1233eecf3957b763576afd0f3aa8ad5f9288
Prabinprasanga32 <rautkedar1970@gmail.com>
2021-06-24 18:00:06 +0545

    message is for commiting

--- resources/views/AdminUI/product.blade.php
+++ resources/views/AdminUI/product.blade.php
@@ -6,7 +6,7 @@
       <div class="container-fluid">
         <div class="row mb-2">
           <div class="col-sm-6">
-            <h1>Blank Page</h1>
+            <h1>This is not a blank page hahaha</h1>
           </div>
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
