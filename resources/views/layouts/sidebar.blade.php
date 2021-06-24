de1264be4a567aab4cb4d2f306f2913f4bd629f5
Bhuwanadhikari332 <65113297+Bhuwanadhikari332@users.noreply.github.com>
2021-06-24 18:08:52 +0545

    initial commit by bhuwan

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -2,7 +2,7 @@
     <!-- Brand Logo -->
     <a href="index3.html" class="brand-link">
       <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
-      <span class="brand-text font-weight-light">Online Itahari</span>
+      <span class="brand-text font-weight-light">AdminLTE 3</span>
     </a>
 
     <!-- Sidebar -->
@@ -38,7 +38,7 @@
                 <a href="/" class="nav-link">
                   <i class="nav-icon fas fa-th"></i>
                   <p>
-                    Dashboard
+                    Daaash 2
                   </p>
                 </a>
               </li>
